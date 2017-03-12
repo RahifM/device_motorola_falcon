0d5ff779ee7ae23e1d927e24e24e141949392ecb
RahifM <faizel326@gmail.com>
2017-03-12 10:48:31 +0000

    falcon: AOSPA Bring-Up
    Signed-off-by: RahifM <faizel326@gmail.com>

--- full_falcon.mk
+++ full_falcon.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from falcon device
 $(call inherit-product, device/motorola/falcon/device.mk)
