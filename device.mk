ac20314a14a0e9f61ebe2ffdddb09fd6586f1fef
Gabriele M <moto.falcon.git@gmail.com>
2017-03-22 18:27:48 +0100

    Revert "Switch to open source AKM daemon"
    The conevrtion factors must be adjusted, the compass sensor is
    reporting incorrect data with the OSS daemon. Go back to the
    prebuilt daemon for now.
    
    This reverts commit cf546934c915c7ddbbaaf6abe42b02b45568d7a7.
    
    Change-Id: Ide69aed7b1143d3a51c6458c5eaef30d236b45ff

--- device.mk
+++ device.mk
@@ -29,10 +29,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.target.rc
 
-# Sensors
-PRODUCT_PACKAGES += \
-    akmd8963
-
 # Wifi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
