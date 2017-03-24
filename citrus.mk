c09eaa39b3131bb858735362f1d782d23753b037
RahifM <faizel326@gmail.com>
2017-03-24 19:39:45 +0000

    falcon: Citrus-ify
    Signed-off-by: RahifM <faizel326@gmail.com>

---
+++ citrus.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+
+# Inherit some common shits!
+$(call inherit-product, vendor/citrus/common.mk)
+
+PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_NAME := citrus_falcon
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+CITRUS_RELEASE := true
+
+PRODUCT_PROPERTY_OVERRIDES += ro.caf.branch=LA.UM.5.6.r1-03400-89xx.0
+
+# Citrus-CAF Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+         DEVICE_MAINTAINERS="Rahif M"
