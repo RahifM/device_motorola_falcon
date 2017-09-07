8a3271d776eef062755a3ad7c001ab0e423156e1
RahifM <faizel326@gmail.com>
2017-09-07 16:01:08 +0000

    falcon: AOSP Bringup
    Signed-off-by: RahifM <faizel326@gmail.com>

---
+++ aosp_falcon.mk
@@ -0,0 +1,12 @@
+# Grab em APNs
+PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+
+PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_NAME := aosp_falcon
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_PACKAGES += \
+    Launcher3
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo aosp_falcon-eng
+add_lunch_combo aosp_falcon-userdebug
