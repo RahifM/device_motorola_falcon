7ce087677755027224b9da8db635a6e66b5898f9
rahifm <faizel326@gmail.com>
2017-10-15 08:54:49 +0000

    falcon: Make DiRTY
    Signed-off-by: RahifM <faizel326@gmail.com>
    Signed-off-by: innfinite4evr <innfinite4evr@gmail.com>

--- du.mk
+++ du.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/citrus/common.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := citrus_falcon
+PRODUCT_NAME := du_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
