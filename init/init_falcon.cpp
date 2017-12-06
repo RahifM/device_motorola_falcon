bd6906b8e25cae528948cff6ac3a153470a6aca4
Adarsh-MR <adarshmr1998@gmail.com>
2017-12-06 20:40:54 +0000

    falcon: init: fix build on oreo-mr1
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    Signed-off-by: RahifM <faizel326@gmail.com>

--- init/init_falcon.cpp
+++ init/init_falcon.cpp
@@ -66,98 +66,94 @@ void vendor_load_properties()
             property_override("ro.build.description", "falcon_gpe-user 5.1 LMY47M.M005 10 release-keys");
             property_override("ro.build.fingerprint", "motorola/falcon_gpe/falcon_umts:5.1/LMY47M.M005/10:user/release-keys");
             property_override("ro.build.product", "falcon_gpe");
-            property_set("ro.mot.build.customerid", "retusa_glb");
-            property_set("ro.telephony.default_network", "0");
-            property_set("persist.radio.multisim.config", "");
+            android::init::property_set("ro.mot.build.customerid", "retusa_glb");
+            android::init::property_set("ro.telephony.default_network", "0");
+            android::init::property_set("persist.radio.multisim.config", "");
         } else {
             /* xt1032 */
             property_override("ro.product.device", "falcon_umts");
             property_override("ro.build.description", "falcon_retuglb-user 5.1 LPB23.13-58 58 release-keys");
             property_override("ro.build.fingerprint", "motorola/falcon_retuglb/falcon_umts:5.1/LPB23.13-58/58:user/release-keys");
             property_override("ro.build.product", "falcon_umts");
-            property_set("ro.mot.build.customerid", "retusa_glb");
-            property_set("ro.telephony.default_network", "0");
-            property_set("persist.radio.multisim.config", "");
+            android::init::property_set("ro.mot.build.customerid", "retusa_glb");
+            android::init::property_set("ro.telephony.default_network", "0");
+            android::init::property_set("persist.radio.multisim.config", "");
         }
     } else if (radio == "0x3") {
         std::string carrier = GetProperty("ro.boot.carrier", "");
         if (carrier == "vzw") {
             property_override("ro.build.description", "falcon_verizon-user 5.1 LPB23.13-33.7 7 release-keys");
             property_override("ro.build.fingerprint", "motorola/falcon_verizon/falcon_cdma:5.1/LPB23.13-33.7/7:user/release-keys");
-            property_set("ro.mot.build.customerid", "verizon");
-            property_set("ro.cdma.home.operator.alpha", "Verizon");
-            property_set("ro.cdma.home.operator.numeric", "310004");
-            property_set("ro.com.google.clientidbase.ms", "android-verizon");
-            property_set("ro.com.google.clientidbase.am", "android-verizon");
-            property_set("ro.com.google.clientidbase.yt", "android-verizon");
-            property_set("persist.radio.nw_mtu_enabled", "true");
+            android::init::property_set("ro.mot.build.customerid", "verizon");
+            android::init::property_set("ro.cdma.home.operator.alpha", "Verizon");
+            android::init::property_set("ro.cdma.home.operator.numeric", "310004");
+            android::init::property_set("ro.com.google.clientidbase.ms", "android-verizon");
+            android::init::property_set("ro.com.google.clientidbase.am", "android-verizon");
+            android::init::property_set("ro.com.google.clientidbase.yt", "android-verizon");
+            android::init::property_set("persist.radio.nw_mtu_enabled", "true");
         } else if (carrier == "boost") {
             property_override("ro.build.description", "falcon_boost-user 5.1 LPB23.13-56 55 release-keys");
             property_override("ro.build.fingerprint", "motorola/falcon_boost/falcon_cdma:5.1/LPB23.13-56/55:user/release-keys");
-            property_set("ro.mot.build.customerid", "sprint");
-            property_set("ro.cdma.home.operator.alpha", "Boost Mobile");
-            property_set("ro.cdma.home.operator.numeric", "311870");
-            property_set("ro.com.google.clientidbase.ms", "android-boost-us");
-            property_set("ro.com.google.clientidbase.am", "android-boost-us");
-            property_set("ro.com.google.clientidbase.yt", "android-boost-us");
+            android::init::property_set("ro.mot.build.customerid", "sprint");
+            android::init::property_set("ro.cdma.home.operator.alpha", "Boost Mobile");
+            android::init::property_set("ro.cdma.home.operator.numeric", "311870");
+            android::init::property_set("ro.com.google.clientidbase.ms", "android-boost-us");
+            android::init::property_set("ro.com.google.clientidbase.am", "android-boost-us");
+            android::init::property_set("ro.com.google.clientidbase.yt", "android-boost-us");
         } else if (carrier == "usc") {
             property_override("ro.build.description", "falcon_usc-user 5.1 LPB23.13-33.6 8 release-keys");
             property_override("ro.build.fingerprint", "motorola/falcon_usc/falcon_cdma:5.1/LPB23.13-33.6/8:user/release-keys");
-            property_set("ro.mot.build.customerid", "usc");
-            property_set("ro.cdma.home.operator.numeric", "311220");
-            property_set("gsm.sim.operator.numeric", "311580");
-            property_set("ro.cdma.home.operator.alpha", "U.S. Cellular");
-            property_set("ro.com.google.clientidbase.ms", "android-uscellular-us");
-            property_set("ro.com.google.clientidbase.am", "android-uscellular-us");
-            property_set("ro.com.google.clientidbase", "android-motorola");
-            property_set("ro.com.google.clientidbase.gmm", "android-motorola");
-            property_set("ro.com.google.clientidbase.yt", "android-motorola");
-        } else {
-            LOG(ERROR) << "Unknown mobile carrier";
+            android::init::property_set("ro.mot.build.customerid", "usc");
+            android::init::property_set("ro.cdma.home.operator.numeric", "311220");
+            android::init::property_set("gsm.sim.operator.numeric", "311580");
+            android::init::property_set("ro.cdma.home.operator.alpha", "U.S. Cellular");
+            android::init::property_set("ro.com.google.clientidbase.ms", "android-uscellular-us");
+            android::init::property_set("ro.com.google.clientidbase.am", "android-uscellular-us");
+            android::init::property_set("ro.com.google.clientidbase", "android-motorola");
+            android::init::property_set("ro.com.google.clientidbase.gmm", "android-motorola");
+            android::init::property_set("ro.com.google.clientidbase.yt", "android-motorola");
         }
         property_override("ro.product.device", "falcon_cdma");
         property_override("ro.build.product", "falcon_cdma");
-        property_set("ro.telephony.default_cdma_sub", "1");
-        property_set("ro.telephony.default_network", "4");
-        property_set("ro.telephony.gsm-routes-us-smsc", "1");
-        property_set("persist.radio.vrte_logic", "2");
-        property_set("persist.radio.0x9e_not_callname", "1");
-        property_set("persist.radio.multisim.config", "");
-        property_set("persist.radio.skip_data_check", "1");
-        property_set("persist.ril.max.crit.qmi.fails", "4");
-        property_set("persist.radio.call_type", "1");
-        property_set("persist.radio.mode_pref_nv10", "1");
-        property_set("persist.radio.snapshot_timer", "22");
-        property_set("persist.radio.snapshot_enabled", "1");
-        property_set("ro.cdma.home.operator.isnan", "1");
-        property_set("ro.cdma.otaspnumschema", "SELC,1,80,99");
-        property_set("ro.cdma.data_retry_config", "max_retries=infinite,0,0,10000,10000,100000,10000,10000,10000,10000,140000,540000,960000");
-        property_set("ro.gsm.data_retry_config", "default_randomization=2000,max_retries=infinite,1000,1000,80000,125000,485000,905000");
-        property_set("ro.mot.ignore_csim_appid", "true");
-        property_set("telephony.lteOnCdmaDevice", "0");
+        android::init::property_set("ro.telephony.default_cdma_sub", "1");
+        android::init::property_set("ro.telephony.default_network", "4");
+        android::init::property_set("ro.telephony.gsm-routes-us-smsc", "1");
+        android::init::property_set("persist.radio.vrte_logic", "2");
+        android::init::property_set("persist.radio.0x9e_not_callname", "1");
+        android::init::property_set("persist.radio.multisim.config", "");
+        android::init::property_set("persist.radio.skip_data_check", "1");
+        android::init::property_set("persist.ril.max.crit.qmi.fails", "4");
+        android::init::property_set("persist.radio.call_type", "1");
+        android::init::property_set("persist.radio.mode_pref_nv10", "1");
+        android::init::property_set("persist.radio.snapshot_timer", "22");
+        android::init::property_set("persist.radio.snapshot_enabled", "1");
+        android::init::property_set("ro.cdma.home.operator.isnan", "1");
+        android::init::property_set("ro.cdma.otaspnumschema", "SELC,1,80,99");
+        android::init::property_set("ro.cdma.data_retry_config", "max_retries=infinite,0,0,10000,10000,100000,10000,10000,10000,10000,140000,540000,960000");
+        android::init::property_set("ro.gsm.data_retry_config", "default_randomization=2000,max_retries=infinite,1000,1000,80000,125000,485000,905000");
+        android::init::property_set("ro.mot.ignore_csim_appid", "true");
+        android::init::property_set("telephony.lteOnCdmaDevice", "0");
     } else if (radio == "0x5") {
         /* xt1033 */
         property_override("ro.product.device", "falcon_umtsds");
         property_override("ro.build.description", "falcon_retbr_ds-user 5.1 LPB23.13-56 58 release-keys");
         property_override("ro.build.fingerprint", "motorola/falcon_retbr_ds/falcon_umtsds:5.1/LPB23.13-56/58:user/release-keys");
         property_override("ro.build.product", "falcon_umtsds");
-        property_set("ro.mot.build.customerid", "RETBR");
-        property_set("ro.telephony.default_network", "0,1");
-        property_set("ro.telephony.ril.config", "simactivation,sim2gsmonly");
-        property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.radio.dont_use_dsd", "true");
-        property_set("persist.radio.plmn_name_cmp", "1");
+        android::init::property_set("ro.mot.build.customerid", "RETBR");
+        android::init::property_set("ro.telephony.default_network", "0,1");
+        android::init::property_set("ro.telephony.ril.config", "simactivation,sim2gsmonly");
+        android::init::property_set("persist.radio.multisim.config", "dsds");
+        android::init::property_set("persist.radio.dont_use_dsd", "true");
+        android::init::property_set("persist.radio.plmn_name_cmp", "1");
     } else if (radio == "0x6") {
         /* xt1034 */
         property_override("ro.product.device", "falcon_umts");
         property_override("ro.build.description", "falcon_retuaws-user 5.1 LPB23.13-58 61 release-keys");
         property_override("ro.build.fingerprint", "motorola/falcon_retuaws/falcon_umts:5.1/LPB23.13-58/61:user/release-keys");
         property_override("ro.build.product", "falcon_umts");
-        property_set("ro.mot.build.customerid", "retusa_aws");
-        property_set("ro.telephony.default_network", "0");
-        property_set("persist.radio.multisim.config", "");
+        android::init::property_set("ro.mot.build.customerid", "retusa_aws");
+        android::init::property_set("ro.telephony.default_network", "0");
+        android::init::property_set("persist.radio.multisim.config", "");
     }
 
-    std::string device = GetProperty("ro.product.device", "");
-    LOG(INFO) << "Found radio id: '" << radio.c_str() << "', setting build properties for '" << device.c_str() << "' device\n";
 }
