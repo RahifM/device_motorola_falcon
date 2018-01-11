78d0c29649d4bd68cc52ec36a31090853b630636
RahifM <faizel326@gmail.com>
2018-01-11 14:25:33 +0000

    Bringup Citrus-CAF for falcon
    Signed-off-by: RahifM <faizel326@gmail.com>

--- citrus.mk
+++ citrus.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/cardinal/common.mk)
+$(call inherit-product, vendor/citrus/common.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := cardinal_falcon
+PRODUCT_NAME := citrus_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
